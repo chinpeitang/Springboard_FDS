9842bdbb8af05bc036f28424c4280516b2b9aed8
chinpeitang <cptang@gmail.com>
2016-05-15 11:47:13 -0500

    Separated product codes and numbers, renamed products, created full address column
    Separated "-" between product codes and numbers, used "cbind" to
    rearrange columns, rename products using "grep", combined full address
    using "paste". Still figuring out a good way to create binary columns
    for company and product names.

--- DW_Ex1/DWEx1_Refine.R
+++ DW_Ex1/DWEx1_Refine.R
@@ -23,5 +23,27 @@ summary(refine$company)
 uni_names = grep(pattern = "uni", x = refine$company, ignore.case = T)
 refine$company[uni_names] = "unilever"
 summary(refine$company)
+
+# Separate "-" between codes and numbers
+product_code_number = as.character(refine$Product.code...number)
+product_code_number = strsplit(product_code_number, split = "-")
+product_code = vapply(product_code_number, function(x){x[1]}, character(1))
+product_code
+product_number = vapply(product_code_number, function(x){x[2]}, character(1))
+product_number
+# Rearrange to columns
+refine = cbind(refine[1], product_code, product_number, refine[3:6])
+
+# Rename products
+refine$product_code = as.character(refine$product_code)
+refine$product_code[grep(pattern = "p", x = refine$product_code)] = "Smartphone"
+refine$product_code[grep(pattern = "v", x = refine$product_code)] = "TV"
+refine$product_code[grep(pattern = "x", x = refine$product_code)] = "Laptop"
+refine$product_code[grep(pattern = "q", x = refine$product_code)] = "Tablet"
+
+# Create full address column
+full_address = paste(refine$address, refine$city, refine$country, sep = ", ")
+refine = cbind(refine[1:6], full_address, refine[7])
+
 # Write to clean file
 write.csv(refine, file = "refine_clean.csv")
