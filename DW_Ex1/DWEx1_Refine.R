5aedcecea50191eeb027e6997b74052e80ca332d
chinpeitang <cptang@gmail.com>
2016-05-15 14:34:10 -0500

    Binary variable columns for companies and product categories, some codes cleanup
    Created binary variable columns for companies and product categories.
    Some codes cleanup for company names consolidation. New complete CSV
    file. Data Wrangling Exercise 1 completed.

--- DW_Ex1/DWEx1_Refine.R
+++ DW_Ex1/DWEx1_Refine.R
@@ -3,26 +3,17 @@ setwd("C:/Users/Chinpei/Documents/GitHub/Springboard_FDS/DW_Ex1")
 # Load original data
 refine = read.csv("refine_original.csv", header = T)
 # Check data
-names(refine)
 summary(refine)
-summary(refine$company)
 dim(refine)
-# Consolidate "philips"
-phil_names = grep(pattern = "ps", x = refine$company, ignore.case = T)
-refine$company[phil_names] = "philips"
+
+# Consolidate company names
 summary(refine$company)
-# Consolidate "akzo"
-akzo_names = grep(pattern = "ak", x = refine$company, ignore.case = T)
-refine$company[akzo_names] = "akzo"
-summary(refine$company)
-# Consolidate "van houten"
-van_names = grep(pattern = "van", x = refine$company, ignore.case = T)
-refine$company[van_names] = "van houten"
-summary(refine$company)
-# Consolidate "unilever"
-uni_names = grep(pattern = "uni", x = refine$company, ignore.case = T)
-refine$company[uni_names] = "unilever"
-summary(refine$company)
+# Convert all company names to lower case
+refine$company = vapply(refine$company, tolower, character(1))
+refine$company[grep(pattern = "ps", x = refine$company)] = "philips"
+refine$company[grep(pattern = "ak", x = refine$company)] = "akzo"
+refine$company[grep(pattern = "van", x = refine$company)] = "van houten"
+refine$company[grep(pattern = "uni", x = refine$company)] = "unilever"
 
 # Separate "-" between codes and numbers
 product_code_number = as.character(refine$Product.code...number)
@@ -44,6 +35,18 @@ refine$product_code[refine$product_code == "q"] = "Tablet"
 # Create full address column
 full_address = paste(refine$address, refine$city, refine$country, sep = ", ")
 refine = cbind(refine[1:6], full_address, refine[7])
+
+# Create binary variables columns for companies
+refine$company_philips = as.integer(refine$company == "philips")
+refine$company_akzo = as.integer(refine$company == "akzo")
+refine$company_van_houten = as.integer(refine$company == "van houten")
+refine$company_unilever = as.integer(refine$company == "unilever")
+
+# Create binary variables columns for product categories
+refine$product_smartphone = as.integer(refine$product_code == "Smartphone")
+refine$product_tv = as.integer(refine$product_code == "TV")
+refine$product_laptop = as.integer(refine$product_code == "Laptop")
+refine$product_tablet = as.integer(refine$product_code == "Tablet")
 
 # Write to clean file
 write.csv(refine, file = "refine_clean.csv")
